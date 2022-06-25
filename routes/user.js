46ea364a48fce55d1690c75b42f7b18c32c5f771
unknown <abusayed10809@gmail.com>
2022-06-25 11:49:33 +0600

    verify token complete

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,28 @@
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const User = require("../models/User");
+
 const router = require("express").Router();
 
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECURITY
+    ).toString();
+  }
 
-module.exports = router
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
---
+++ routes/verifyToken.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.Authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, process.env.JWT_SECRET_KEY, (error, user) => {
+      if (error) {
+        res.status(403).json("Invalid token");
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated. No web token.");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Action is not allowed by user!");
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuthorization };
