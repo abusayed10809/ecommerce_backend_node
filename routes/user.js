ea8331fbc1b1772514356183c7f6c0559858b305
unknown <abusayed10809@gmail.com>
2023-04-11 04:45:11 +0600

    user router complete

--- routes/user.js
+++ routes/user.js
@@ -57,10 +57,12 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
 
 /// get all users --------------------------------
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
   try {
-    const user = await User.find();
-    const { password, ...otherData } = user._doc;
-    res.status(200).json(otherData);
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json(error);
   }
