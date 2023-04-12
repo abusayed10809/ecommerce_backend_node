1c75707d09c3205d7c9894f9bceb10c2be5010ce
unknown <abusayed10809@gmail.com>
2023-04-13 05:53:15 +0600

    order route complete

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running on port 5000");
--- routes/order.js
+++ routes/order.js
@@ -1,4 +1,130 @@
+const Order = require("../models/Order");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
 const router = require("express").Router();
 
+/* 
+create new order ----------------------------------------------------------------
+- when user will checkout
+*/
+router.post("/", verifyToken, async (req, res) => {
+  try {
+    const newOrder = new Order(req.body);
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-module.exports = router
+/* 
+UPDATE ----------------------------------------------------------------
+- only admin can update a created order
+- id params ->>> order id
+*/
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/* 
+DELETE ----------------------------------------------------------------
+- id param ->>> order id
+ */
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/* 
+GET USER ORDER ------------------------------------------------------
+- show all user orders
+*/
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const orders = await Order.find({
+      userId: req.params.userId,
+    });
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/* 
+GET ALL ----------------------------------------------------------------
+ */
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/* 
+GET MONTHLY INCOME ----------------------------------------------------------
+*/
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(
+      new Date().setMonth(lastMonth.getMonth() - 1)
+    );
+    const income = await Order.aggregate([
+      // filter by the last two months
+      // date greater than current month - 2 months
+      {
+        $match: {
+          createdAt: {
+            $gte: previousMonth,
+          },
+        },
+      },
+      // create two new variables
+      // month will hold the month values extracting month from createdAt date
+      // sales will hold the amount values
+      {
+        $project: {
+          month: {
+            $month: "$createdAt",
+          },
+          sales: "$amount",
+        },
+      },
+      // group by month value
+      // total will hold the sum of all sales value for that month
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
+    ]);
+    res.status(200).json(income);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
