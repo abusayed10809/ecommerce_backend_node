a4c04a72418a42377057e7b54f4bf51154405443
unknown <abusayed10809@gmail.com>
2023-04-14 05:43:22 +0600

    stripe payment method implemented

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -20,12 +22,14 @@ mongoose
     console.log("error occured " + error);
   });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running on port 5000");
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(
+  "sk_test_51MwZWrD4Mmtyc8EQOA1rc0BP71KwcgKxVTpnoCiadza3xbATxFaDXk9roHKK05NaG2KRfpzjd4GrzumSDIYc3iEZ00QXR28mhR"
+);
 
 router.post("/payment", (req, res) => {
+  console.log("stripe payment api request");
   stripe.charges.create(
     {
       source: req.body.tokenId,
@@ -10,6 +14,8 @@ router.post("/payment", (req, res) => {
     },
     (stripeErr, stripeRes) => {
       if (stripeErr) {
+        console.log("stripe error");
+        console.log(stripeErr);
         res.status(500).json(stripeErr);
       } else {
         res.status(200).json(stripeRes);
