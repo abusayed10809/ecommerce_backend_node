031aecd18adb538df2e532599b8d863d84a246d2
unknown <abusayed10809@gmail.com>
2023-04-13 23:40:05 +0600

    stripe payment method WIP

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 
@@ -23,7 +24,8 @@ app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
-app.use("/api/cart", cartRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running on port 5000");
